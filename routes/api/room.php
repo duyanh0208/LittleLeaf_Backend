39d787a1d4675bfe85938216c782bac930d39278
Duy Anh <21020263@vnu.edu.vn>
2023-11-21 15:13:21 +0700

    init

--- routes/api/room.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\RoomController;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MessageController;
-
-Route::get('/rooms/{id}', [RoomController::class, "index"])->middleware("jwt.verify");
